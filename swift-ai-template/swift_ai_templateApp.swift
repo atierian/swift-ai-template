0dd3a0c99eec1cf4e8c2b13d8e0f6d12d8074103
Ian Saultz <52051793+atierian@users.noreply.github.com>
2024-09-19 22:50:12 -0400

    Initial Commit

---
+++ swift-ai-template/swift_ai_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ai_templateApp.swift
+//  swift-ai-template
+//
+//  Created by Saultz, Ian on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ai_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-ai-templateTests/swift_ai_templateTests.swift
@@ -0,0 +1,16 @@
+//
+//  swift_ai_templateTests.swift
+//  swift-ai-templateTests
+//
+//  Created by Saultz, Ian on 9/19/24.
+//
+
+import Testing
+
+struct swift_ai_templateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
